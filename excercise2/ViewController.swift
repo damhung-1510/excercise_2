c272b4ba6fb384f67ced187bf2837b3c2a146ccd
hungdq <admin@Hungdq198.local>
2024-04-01 18:35:20 +0700

    done animation

--- excercise2/ViewController.swift
+++ excercise2/ViewController.swift
@@ -16,36 +16,47 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var contentView: UIView!
     
-    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var contentHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var contentTopContraint: NSLayoutConstraint!
     
     var isExpaned: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.addSubview(mainView)
-        mainView.addSubview(headerView)
-        mainView.heightAnchor.constraint(equalToConstant: 120).isActive = true
-        mainView.backgroundColor = UIColor.red
-        mainView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        mainView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-        contentView.isHidden = true
         
+        contentView.layer.cornerRadius = 8
+        contentView.clipsToBounds = true
+        contentView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
         headerView.addGestureRecognizer(tap)
     }
    
     
     @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
-//        mainView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
-     
+        isExpaned = !isExpaned;
+        UIView.animate(withDuration: 0.5) {
+            if self.isExpaned {
+                self.animating(headerHeight: 60, contentHeightConstraint: 0, contentTopContraint: 60)
+            } else {
+                self.animating(headerHeight: 120, contentHeightConstraint: 220, contentTopContraint: 120)
+            }
             
-        UIView.transition(with: contentView, duration: 1,
-                              options: .curveLinear,
-                              animations: {
-            self.contentView.isHidden = !self.contentView.isHidden
-                          })
-       
+        }
+    }
+    
+    func animating(headerHeight: CGFloat, contentHeightConstraint: CGFloat, contentTopContraint:CGFloat ){
+        self.headerHeightConstraint.constant = headerHeight
+        self.contentHeightConstraint.constant = contentHeightConstraint
+        self.contentTopContraint.constant = contentTopContraint;
+        self.headerView.layoutIfNeeded()
+        self.contentView.layoutIfNeeded()
+        self.containerView.layoutIfNeeded()
     }
 
 }
